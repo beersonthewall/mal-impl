8b02d6079f80ae72842fb0b2c1f0f54c694ce8a9
beersonthewall <matthew@beersonthewall.com>
2021-07-25 07:44:34 -0500

    Display impl for MalType, MalList, and MalAtom.

--- src/types.rs
+++ src/types.rs
@@ -1,12 +1,41 @@
+use std::fmt;
+
 #[derive(Debug, PartialEq)]
 pub enum MalType {
     List(MalList),
     Atom(MalAtom),
 }
 
+impl fmt::Display for MalType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            MalType::List(mal_list) => mal_list.fmt(f),
+            MalType::Atom(mal_atom) => mal_atom.fmt(f),
+        }
+    }
+}
+
 #[derive(Debug, PartialEq)]
 pub struct MalList {
     elements: Vec<MalType>,
+}
+
+impl fmt::Display for MalList {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "( ")?;
+        let mut itr = self.elements.iter();
+
+        if let Some(elem) = itr.next() {
+            elem.fmt(f)?;
+        }
+
+        for elem in itr {
+            write!(f, ", ")?;
+            elem.fmt(f)?;
+        }
+        
+        write!(f, " )")
+    }
 }
 
 impl MalList {
@@ -19,4 +48,13 @@ impl MalList {
 pub enum MalAtom {
     Int(isize),
     Symbol(String),
+}
+
+impl fmt::Display for MalAtom {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            MalAtom::Int(integer) => write!(f, "{}", integer),
+            MalAtom::Symbol(string) => write!(f, "{}", string),
+        }
+    }
 }
