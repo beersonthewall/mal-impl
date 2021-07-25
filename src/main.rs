c1cb663e2614496759bbe1b68a3dd64bdc414307
beersonthewall <matthew@beersonthewall.com>
2021-07-25 08:14:35 -0500

    Update read eval print loop to use the Display impls for MalType, MalList, and MalAtom.

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,20 @@ mod tokenizer;
 mod types;
 
 use reader::read_str;
+use types::MalType;
 
-pub fn read(input: &str) -> &str {
-    read_str(&input);
+pub fn read(input: &str) -> Option<MalType> {
+    read_str(&input)
+}
+
+fn eval(input: Option<MalType>) -> Option<MalType> {
     input
 }
 
-fn eval(input: &str) -> &str {
-    return input;
-}
-
-fn print(input: &str) -> &str {
-    return input;
+fn print(input: Option<MalType>) {
+    if let Some(mal_type) = input {
+        println!("{}", mal_type);
+    }
 }
 
 fn main() {
@@ -25,7 +27,7 @@ fn main() {
         match rl.readline("user> ") {
             Ok(line) => {
                 rl.add_history_entry(line.as_str());
-                println!("{}", print(eval(read(&line))));
+                print(eval(read(&line)));
             }
             Err(_) => break,
         }
